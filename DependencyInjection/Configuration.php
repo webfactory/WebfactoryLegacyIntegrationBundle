5bafb6f6ef6bdb50783d249973f75e500126c0c7
Per Bernhardt <plb@webfactory.de>
2012-07-09 06:31:24 -0200

    Konfiguration implementiert

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Webfactory\Bundle\LegacyIntegrationBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $treeBuilder->root('webfactory_legacy_integration')
+            ->children()
+                ->scalarNode('legacyApplicationBootstrapFile')->defaultValue('%project.webdir%/wfD2Engine.php')->end()
+                ->scalarNode('strategy')->defaultValue('recycling')->end();
+        return $treeBuilder;
+    }
+}
--- DependencyInjection/WebfactoryLegacyIntegrationExtension.php
+++ DependencyInjection/WebfactoryLegacyIntegrationExtension.php
@@ -9,16 +9,26 @@ use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class WebfactoryLegacyIntegrationExtension extends Extension {
 
-    public function load(array $config, ContainerBuilder $container) {
-        $container->setParameter('webfactory.legacy_integration.legacy_application_bootstrap_file', 'wfD2Engine.php');
+    public function load(array $configs, ContainerBuilder $container) {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $container->setParameter('webfactory.legacy_integration.legacy_application_bootstrap_file', $config['legacyApplicationBootstrapFile']);
+
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
 
-        if (false /** In der Konfiguration ist die "Recycling"-Integration gewählt */) {
-            $container->removeDefinition('webfactory.legacy_integration.retrofitting_event_listener');
-            $container->removeDefinition('webfactory.legacy_integration.symfony_application');
-        } else {
-            $container->removeDefinition('webfactory.legacy_integration.recycling_event_listener');
+        switch ($config['strategy']) {
+            case 'recycling':
+                $container->removeDefinition('webfactory.legacy_integration.retrofitting_event_listener');
+                $container->removeDefinition('webfactory.legacy_integration.symfony_application');
+                break;
+            case 'retrofitting':
+                $container->removeDefinition('webfactory.legacy_integration.recycling_event_listener');
+                break;
+            default:
+                throw new \Symfony\Component\DependencyInjection\Exception\RuntimeException("Unbekannte Integrationsstrategie {$config['strategy']}.");
+                break;
         }
     }
 
