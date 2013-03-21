090ba674b9314fbbc60912379c440698a2b36616
Unknown <unknown@kiln.example.com>
2013-03-21 13:33:49 -0100

    Case 17728: Das wiederverwendbar gemacht, was ich fuer den Moment brauche :)

--- Integration/LegacyApplication.php
+++ Integration/LegacyApplication.php
@@ -24,40 +24,12 @@ class LegacyApplication extends IntegratableApplication {
     public function dispatch() {
         if (!$this->dispatched) {
             $this->dispatched = true;
+
             $legacyBootstrap = $this->bootstrapFile;
 
-            ob_start();
-            include($legacyBootstrap);
-
-            if (headers_sent())
-                throw new \Exception("Du musst sicherstellen, dass die Legacy-Anwendung $legacyBootstrap keine Header bzw. Output sendet (output_buffering)!");
-
-            /**
-             * http_response_code geht leider erst mit PHP5.4.
-             * Wir wollen aber eigentlich sowieso, dass alle Responses <> 200 mit Exceptions realisiert werden.
-             * Die Exception Page wird über den Kernel gelöst
-             */
-            $statusCode = 200;
-
-            $content = ob_get_contents();
-            ob_end_clean();
-
-            $headers = headers_list();
-            $responseHeaders = array();
-            foreach ($headers as $header) {
-                $header = preg_match('(^([^:]+):(.*)$)', $header, $matches);
-                $headerName = trim($matches[1]);
-                $headerValue = trim($matches[2]);
-                $responseHeaders[$headerName][] = $headerValue;
-            }
-            header_remove();
-            
-            if (isset($responseHeaders['Location'])) {
-                $statusCode = '302';
-                unset($responseHeaders['Expires']);
-            } 
-            
-            $this->response = new Response($content, $statusCode, $responseHeaders);                
+            $this->response = LegacyCaptureResponseFactory::create(function() use ($legacyBootstrap) {
+                include($legacyBootstrap);
+            });
         }
     }
 
---
+++ Integration/LegacyCaptureResponseFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Webfactory\Bundle\LegacyIntegrationBundle\Integration;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+class LegacyCaptureResponseFactory {
+
+    public static function create($legacyExecutionCallback) {
+        ob_start();
+        $statusCode = call_user_func($legacyExecutionCallback) ? : 200;
+
+        if (headers_sent())
+            throw new \RuntimeException("It must be possible to caputure the legacy application's output with ob_start(). Headers and/or output must not have been sent to the client.");
+
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        $headers = headers_list();
+        header_remove();
+
+        $responseHeaders = array();
+
+        foreach ($headers as $header) {
+            $header = preg_match('(^([^:]+):(.*)$)', $header, $matches);
+            $headerName = trim($matches[1]);
+            $headerValue = trim($matches[2]);
+            $responseHeaders[$headerName][] = $headerValue;
+        }
+
+        if (isset($responseHeaders['Location'])) {
+            unset($responseHeaders['Expires']);
+            return new RedirectResponse($responseHeaders['Location'][0], 302, $responseHeaders);
+        }
+
+        return new Response($content, $statusCode, $responseHeaders);
+    }
+
+}
