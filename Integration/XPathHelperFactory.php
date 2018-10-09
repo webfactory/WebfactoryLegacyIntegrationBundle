da6ebfabde5f77a6c6589c0699f5cea6afca1ac9
Matthias Pigulla <mp@webfactory.de>
2018-10-09 11:59:01 +0200

    Add last-ditch attempt to recover from invalid XHTML when things go south

--- Integration/XPathHelperFactory.php
+++ Integration/XPathHelperFactory.php
@@ -8,25 +8,47 @@
 
 namespace Webfactory\Bundle\LegacyIntegrationBundle\Integration;
 
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use Webfactory\Dom\BaseParsingHelper;
+use Webfactory\Dom\Exception\ParsingException;
 
 class XPathHelperFactory
 {
-
     /** @var BaseParsingHelper */
     protected $parser;
 
     /** @var LegacyApplication */
     protected $legacyApplication;
 
-    public function __construct(BaseParsingHelper $parser, LegacyApplication $legacy)
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(BaseParsingHelper $parser, LegacyApplication $legacy, LoggerInterface $logger = null)
     {
         $this->parser = $parser;
         $this->legacyApplication = $legacy;
+        $this->logger = $logger ?: new NullLogger();
     }
 
     public function createHelper()
     {
-        return new XPathHelper($this->parser, $this->legacyApplication->getResponse()->getContent());
+        $content = $this->legacyApplication->getResponse()->getContent();
+
+        try {
+            return new XPathHelper($this->parser, $content);
+        } catch (ParsingException $exception) {
+            if (function_exists('tidy_repair_string')) {
+                $this->logger->notice('Failed parsing the legacy response as XHTML, trying to clean up with Tidy.', ['exception' => $exception, 'legacy_response' => $exception->getXmlInput()]);
+
+                return new XPathHelper($this->parser, tidy_repair_string($content, ['output-xhtml' => true], 'utf8'));
+            } else {
+                $this->logger->warning('Failed to process legacy response as XHTML.', ['exception' => $exception, 'legacy_response' => $exception->getXmlInput()]);
+
+                throw $exception;
+            }
+        }
     }
 }
